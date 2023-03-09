ba00b796710ecf28758484a981ac29901474c22d
Paul <pasha.khimich@mail.ru>
2023-03-09 13:34:38 +0300

    smth

--- src/index.js
+++ src/index.js
@@ -1,34 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Space, Layout, Col, Row} from 'antd';
+import {Space, Layout, Col, Row, Card} from 'antd';
+import pic from './testPic.png'
+import { Content } from 'antd/es/layout/layout';
 
-const {Content} = Layout
 
-class App extends React.Component {
-  render() {
-    function rawCompontent() {
-      let arr = [];
-      for(let i = 0; i <= 3; i++) {
-        arr.push(<Row key={Math.random()}>
-          <Col span='12'></Col>
-          <Col span='12'></Col>
-        </Row>)
-      }
-      return arr
+
+async function getData() {
+let newRequest = await fetch('https://api.themoviedb.org/3/search/movie?api_key=50144123a6271043596e1c7cd112f310&query=return')
+let response =  await newRequest.json();
+    for(let i = 1; i <= 4; i++) {
+        console.log(response.results[i])
     }
-    return (
-      <Space>
-        <Layout>
-          <Content>
-            {rawCompontent()}
-          </Content>
-        </Layout>
-      </Space>
-    )
-  }
 }
 
+getData();
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <Layout>
+                <Content className='mainblock'>
+                    <Row gutter={[36,36]}>
+                        <Col span={12}>
+                            <Card size='medium' className='card' cover={<img src={pic} style={{width: 183}}></img>}>
+                                <div className='content'>
+                                    <p>Hello world</p>
+                                    <p>Bye World</p>
+                                    <p>Some other shit</p>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col span={12}>
+                            <Card size='medium' className='card' cover={<img src={pic} style={{width: 183}}></img>}>
+                                <div className='content'>
+                                    <p>Hello world</p>
+                                    <p>Bye World</p>
+                                    <p>Some other shit</p>
+                                </div>
+                            </Card>
+                        </Col>
+
+                        <Col span={12}>
+                            <Card size='medium' className='card' cover={<img src={pic} style={{width: 183}}></img>}>
+                                <div className='content'>
+                                    <p>Hello world</p>
+                                    <p>Bye World</p>
+                                    <p>Some other shit</p>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col span={12}>
+                            <Card size='medium' className='card' cover={<img src={pic} style={{width: 183}}></img>}>
+                                <div className='content'>
+                                    <p>Hello world</p>
+                                    <p>Bye World</p>
+                                    <p>Some other shit</p>
+                                </div>
+                            </Card>
+                        </Col>
+
+                        <Col span={12}>
+                            <Card size='medium' className='card' cover={<img src={pic} style={{width: 183}}></img>}>
+                                <div className='content'>
+                                    <p>Hello world</p>
+                                    <p>Bye World</p>
+                                    <p>Some other shit</p>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col span={12}>
+                            <Card size='medium' className='card' cover={<img src={pic} style={{width: 183}}></img>}>
+                                <div className='content'>
+                                    <p>Hello world</p>
+                                    <p>Bye World</p>
+                                    <p>Some other shit</p>
+                                </div>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Content>
+            </Layout>
+        )
+    }
+}
 
 let root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App/>)
 
-root.render(<App/>);
+import '../src/myStile.css';
