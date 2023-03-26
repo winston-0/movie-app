9ce70a8df36ab4ed9762627f353e3fad90df02e7
Paul <pasha.khimich@mail.ru>
2023-03-27 02:44:49 +0300

    f

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Card, Image, Space } from "antd";
 import RateModule from "../RateModule/RateModule";
 import { GenreConsumer } from "../GenreProvider/GenreProvider";
+import { isArray } from "lodash";
 
 export default class MovieCards extends React.Component {
   render() {
-    const { data, onUpdateData, loading, genreIds } = this.props;
+    const { data, onUpdateData, loading } = this.props;
+    const {genreIds} = data
     const colorForAverageRating = () => {
       if(data.averageVote > 0 && data.averageVote <= 3) {return "movie-card__average-rating--red"}
       else if(data.averageVote > 3 && data.averageVote <= 5) {return "movie-card__average-rating--orange"}
@@ -13,11 +15,10 @@ export default class MovieCards extends React.Component {
       else if(data.averageVote > 7) {return "movie-card__average-rating--green"}
     }
     const getGenres = (genreList) => {
-      console.log(isArray(genreList))
       const result = genreList.map(item => {
         for(let i of genreIds) {
-          if(i in item) {
-            return <span key={Math.random() * 100} className="movie-card__genre">{item[i]}</span>
+          if(i === item.id) {
+            return <span key={Math.random() * 100} className="movie-card__genre">{item.name}</span>
           }
         }
       })
@@ -25,7 +26,7 @@ export default class MovieCards extends React.Component {
     }
     return (
       <GenreConsumer>
-        {(genreList) => {
+        {(genreList) => { return (
           <Card
           loading={loading}
           className="movie-card"
@@ -43,13 +44,14 @@ export default class MovieCards extends React.Component {
             <div className={`movie-card__average-rating ${colorForAverageRating()}`}>{data.averageVote}</div>
             </Space>
             <span className="movie-card__date">{data.date}</span>
-            <Space>
+            <Space wrap>
               {getGenres(genreList)}
             </Space>
             <span className="movie-card__overview">{data.overview}</span>
             <RateModule onUpdateData={onUpdateData} id={data.id} />
           </section>
         </Card>
+        )
         }}
         </GenreConsumer>
     );
